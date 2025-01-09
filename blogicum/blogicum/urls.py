21e266bf9ff1b5cc2306895f99b4a24299b054f3
Udav <udavimpinf@gmail.com>
2025-01-10 00:32:20 +0300

    Подключение правил и описания

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('', include('blog.urls', namespace='homepage')),
     path('posts/<int:id>/', include('blog.urls', namespace='homepage1')),
     path('category/<slug:category_slug>/', include('blog.urls', namespace='homepage2')),
-    path('pages/about/', include('blog.urls', namespace='homepage3')),
-    path('pages/rules/', include('blog.urls', namespace='homepage4')),
+    path('pages/', include('pages.urls', namespace='pages')),
 ]
