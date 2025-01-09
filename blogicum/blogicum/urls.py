83574a6b964f1a31c278d6a71036bbf718172752
Udav <udavimpinf@gmail.com>
2025-01-09 23:59:38 +0300

    ссылки, статика, шаблоны

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -5,5 +5,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('blog.urls', namespace='homepage')),
-    path('admin/', admin.site.urls),
+    path('posts/<int:id>/', include('blog.urls', namespace='homepage1')),
+    path('category/<slug:category_slug>/', include('blog.urls', namespace='homepage2')),
+    path('pages/about/', include('blog.urls', namespace='homepage3')),
+    path('pages/rules/', include('blog.urls', namespace='homepage4')),
 ]
