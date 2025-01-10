f8c5bf6203e4a73c94eb9fc1eb921300bd7fc66a
Udav <udavimpinf@gmail.com>
2025-01-10 12:06:44 +0300

    пред_ревью

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -43,7 +43,8 @@ posts = [
 ]
 def index(request):
     template = 'blog/index.html'
-    return render(request, template)
+    context = {'posts': posts}
+    return render(request, template, context)
 
 def post_detail(request, pk):
     template = 'blog/detail.html'
